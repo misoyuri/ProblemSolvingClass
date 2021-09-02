6d931b1038bbe42ea789877d3d122b9738d49a6e
MisoYuri <oraclemiso@naver.com>
2021-09-02 22:16:37 +0900

    fit to example answers

--- P1.cpp
+++ P1.cpp
@@ -8,10 +8,12 @@ using namespace std;
 int subSolve(string numbers){
     int len = numbers.length();
     int ret = 0;
+    // cout << endl << "In Sub::" << numbers <<endl;
     for(int i = 2; i <= len; i++){
+        cout <<(int)pow(10, len - i) * (int)pow(9, i - 2) << endl;
         ret += (int)pow(10, len - i) * (int)pow(9, i - 2);
     }
-    cout << "In sub::" << numbers << " | " << ret << endl;
+    // cout << "In sub::" << numbers << " | " << ret << endl;
     
     return ret;
 }
@@ -19,14 +21,16 @@ int subSolve(string numbers){
 int solve(string brokenCounter){
     int ret = 0;
     int len = brokenCounter.length() - 1;
-    for(int i = 0 ; i < len; i++){
+    for(int i = 0 ; i <= len; i++){
+        int cur = 0;
         if(brokenCounter[i] > '4'){
-            ret += pow(10, len - i);
-            ret += (brokenCounter[i] - '0' - 1) * subSolve(to_string((int)pow(10, len - i)));
+            cur += pow(10, len - i);
+            cur += (brokenCounter[i] - '0' - 1) * subSolve(to_string((int)pow(10, len - i)));
         }else{
-            ret += (brokenCounter[i] - '0') * subSolve(to_string((int)pow(10, len - i)));
+            cur += (brokenCounter[i] - '0') * subSolve(to_string((int)pow(10, len - i)));
         }
-        cout << "brokenCounter: " << brokenCounter << "] " << ret << endl;
+        // cout << "brokenCounter: " << (brokenCounter[i] - '0') * ((int)pow(10, len - i)) << "] " << cur << endl;
+        ret += cur;
     }
 
     return ret;
@@ -38,9 +42,9 @@ int main(){
 
     cin >> brokenCounter;
 
-    // fixedCounter = brokenCounter - solve(brokenCounter);
+    fixedCounter = stoi(brokenCounter) - solve(brokenCounter);
     
-    cout << stoi(brokenCounter) - solve(brokenCounter) << endl;
-    // cout << fixedCounter << endl;
+    // cout << stoi(brokenCounter) - solve(brokenCounter) << endl;
+    cout << fixedCounter << endl;
     return 0;
 }
