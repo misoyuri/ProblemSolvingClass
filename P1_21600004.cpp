e71e68668d4c7a1a6dd0d06bf50ff0f4d7adbd0e
misoyuri <21600004@handong.edu>
2021-09-06 01:27:45 +0900

    trying hard to fix bugs....

--- P1_21600004.cpp
+++ P1_21600004.cpp
@@ -1,36 +1,32 @@
 #include <iostream>
 #include <vector>
-#include <string.h>
+#include <string>
 #include <cmath>
 
 using namespace std;
 
-int subSolve(string numbers){
+long subSolve(string numbers){
     int len = numbers.length();
-    int ret = 0;
-    // cout << endl << "In Sub::" << numbers <<endl;
+    long ret = 0;
+
     for(int i = 2; i <= len; i++){
-        cout <<(int)pow(10, len - i) * (int)pow(9, i - 2) << endl;
-        ret += (int)pow(10, len - i) * (int)pow(9, i - 2);
+        ret += (long)pow(10, len - i) * (long)pow(9, i - 2);
     }
-    // cout << "In sub::" << numbers << " | " << ret << endl;
-    
+    cout << "sub::" << numbers << "->" << ret << endl;
     return ret;
 }
 
-int solve(string brokenCounter){
-    int ret = 0;
-    int len = brokenCounter.length() - 1;
-    for(int i = 0 ; i <= len; i++){
-        int cur = 0;
+long solve(string brokenCounter){
+    long ret = 0;
+    int len = brokenCounter.length();
+    for(int i = 0 ; i < len; i++){
+        cout << "main pow::" << (long)pow(10, i) << endl;
         if(brokenCounter[i] > '4'){
-            cur += pow(10, len - i);
-            cur += (brokenCounter[i] - '0' - 1) * subSolve(to_string((int)pow(10, len - i)));
+            ret += (long)pow(10, i);
+            ret += (brokenCounter[i] - '0' - 1) * subSolve(to_string((int)pow(10.0, (double)i)));
         }else{
-            cur += (brokenCounter[i] - '0') * subSolve(to_string((int)pow(10, len - i)));
+            ret += (brokenCounter[i] - '0') * subSolve(to_string((int)pow(10.0, (double)i)));
         }
-        // cout << "brokenCounter: " << (brokenCounter[i] - '0') * ((int)pow(10, len - i)) << "] " << cur << endl;
-        ret += cur;
     }
 
     return ret;
@@ -38,11 +34,11 @@ int solve(string brokenCounter){
 
 int main(){
     string brokenCounter = "";
-    size_t fixedCounter = 0;
+    long fixedCounter = 0;
 
     cin >> brokenCounter;
 
-    fixedCounter = stoi(brokenCounter) - solve(brokenCounter);
+    fixedCounter = stol(brokenCounter) - solve(brokenCounter);
     
     // cout << stoi(brokenCounter) - solve(brokenCounter) << endl;
     cout << fixedCounter << endl;
