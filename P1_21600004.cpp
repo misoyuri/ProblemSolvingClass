2ea8bc812b9baf4c9a65a559887f8bc5d822d6c5
MisoYuri <oraclemiso@naver.com>
2021-09-06 01:54:12 +0900

    correct version

--- P1_21600004.cpp
+++ P1_21600004.cpp
@@ -12,20 +12,18 @@ long subSolve(string numbers){
     for(int i = 2; i <= len; i++){
         ret += (long)pow(10, len - i) * (long)pow(9, i - 2);
     }
-    cout << "sub::" << numbers << "->" << ret << endl;
     return ret;
 }
 
 long solve(string brokenCounter){
     long ret = 0;
-    int len = brokenCounter.length();
-    for(int i = 0 ; i < len; i++){
-        cout << "main pow::" << (long)pow(10, i) << endl;
+    int len = brokenCounter.length() - 1;
+    for(int i = 0 ; i <= len; i++){
         if(brokenCounter[i] > '4'){
-            ret += (long)pow(10, i);
-            ret += (brokenCounter[i] - '0' - 1) * subSolve(to_string((int)pow(10.0, (double)i)));
-        }else{
-            ret += (brokenCounter[i] - '0') * subSolve(to_string((int)pow(10.0, (double)i)));
+            ret += (long)pow(10, len - i);
+            ret += (brokenCounter[i] - '0' - 1) * subSolve(to_string((int)pow(10, len -i)));
+        }else if(brokenCounter[i] != '0'){
+            ret += (brokenCounter[i] - '0') * subSolve(to_string((int)pow(10, len - i)));
         }
     }
 
@@ -39,8 +37,6 @@ int main(){
     cin >> brokenCounter;
 
     fixedCounter = stol(brokenCounter) - solve(brokenCounter);
-    
-    // cout << stoi(brokenCounter) - solve(brokenCounter) << endl;
     cout << fixedCounter << endl;
     return 0;
 }
