4fdcc0519cfe2c34c58d235f9015a27c87c9845c
misoyuri <21600004@handong.edu>
2021-10-05 15:45:14 +0900

    P4 wrong answer

--- P4_21600004.cpp
+++ P4_21600004.cpp
@@ -14,53 +14,49 @@ Function *f, *g;
 int f_size, g_size;
 int p, q;
 
-long long sum_AP(int a, int l){
-    printf("\n a: %d, l: %d\n", a, l);
-    return (l - a + 1);
+long long calc_max_function_ret(int a, int l, int val){
+    if(l > q) l = q+1;
+    long long range = abs(l == a ? 1 : l-a);
+    printf("   a:%d, l:%d | val: %d => %lld     ", a, l, val, range*val);
+    return range * val;
 }
 
 int solve(){
     long long ret = 0, subSum = 0;
     int idx_f = 0, idx_g = 0;
-    while(p > f[idx_f].end){
-        // printf("[init] f:[%d, %d] -->", f[idx_f].start, f[idx_f].end);
-        idx_f++;
-        // printf("[init] f:[%d, %d] \n", f[idx_f].start, f[idx_f].end);
-    } 
-    // cout << "===================================" << endl;
-    while(p > g[idx_g].end) {
-        // printf("[init] g:[%d, %d] -->", g[idx_g].start, g[idx_g].end);
-        idx_g++;
-        // printf("[init] g:[%d, %d] \n", g[idx_g].start, g[idx_g].end);
-    } 
+
+    while(p > f[idx_f].end) {idx_f++;} 
+    while(p > g[idx_g].end) {idx_g++;} 
 
     f[idx_f].start = p;
     g[idx_g].start = p;
 
     while(idx_f <= f_size && idx_g <= g_size){
-        if(f[idx_f].end > q) f[idx_f].end = q;
-        if(g[idx_g].end > q) g[idx_g].end = q;
+        // if(f[idx_f].end > q) f[idx_f].end = q;
+        // if(g[idx_g].end > q) g[idx_g].end = q;
         printf("f[%d, %d) | g[%d, %d)", f[idx_f].start, f[idx_f].end, g[idx_g].start, g[idx_g].end);
         if(f[idx_f].end > g[idx_g].end){
             if(f[idx_f].val > g[idx_g].val){
-                cout << "  1  :" << sum_AP(f[idx_f].start, f[idx_f].end - 1) << endl;
-                ret += sum_AP(f[idx_f].start, g[idx_g].end - 1) * f[idx_f].val;
+                cout << "  1  ]" ;
+                ret += calc_max_function_ret(f[idx_f].start, g[idx_g].end, f[idx_f].val);
             }else{
-                cout << "  2  " << endl;
-                ret += sum_AP(g[idx_g].start, g[idx_g].end - 1) * g[idx_g].val;
+                cout << "  2  ]" ;
+                ret += calc_max_function_ret(g[idx_g].start, g[idx_g].end, g[idx_g].val);
             }
+            f[idx_f].start = g[idx_g].end;
             idx_g++;
         }else{
             if(f[idx_f].val > g[idx_g].val){
-                cout << "  3  :" << sum_AP(f[idx_f].start, f[idx_f].end - 1) << endl;
-                ret += sum_AP(f[idx_f].start, f[idx_f].end - 1) * f[idx_f].val;
-                g[idx_g].start = f[idx_f].end;
+                cout << "  3  ]" ;
+                ret += calc_max_function_ret(f[idx_f].start, f[idx_f].end, f[idx_f].val);
             }else{
-                cout << "  4  " << endl;
-                ret += sum_AP(g[idx_g].start, g[idx_g].end - 1) * g[idx_g].val;
+                cout << "  4  ]" ;
+                ret += calc_max_function_ret(g[idx_g].start, f[idx_f].end, g[idx_g].val);
             }
+            g[idx_g].start = f[idx_f].end;
             idx_f++;
         }
+        ret %= 10007;
         printf("=> %lld\n", ret);
     }
 
@@ -108,13 +104,14 @@ void print_functions(){
     for(int i = 0; i <= g_size; i++){
         printf("g[%d]:: start: %d | end: %d | val: %d\n", i, g[i].start, g[i].end, g[i].val);
     }
+    cout << "================================================" << endl;
 }
 
 int main(){
     init_functions();
     print_functions();
 
-    cout << "answer::" << solve() << endl;
+    cout << solve() << endl;
 
 }
 
