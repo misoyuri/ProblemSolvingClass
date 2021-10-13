36d45944cdcc8827892f50c3c32eb30d47e29975
misoyuri <21600004@handong.edu>
2021-10-14 00:13:41 +0900

    C5 might complete C5?

--- C5_21600004.cpp
+++ C5_21600004.cpp
@@ -14,6 +14,8 @@ int map[4][4]{0, };
 bool poly[5][4][4]{false, };
 Pair pairs[5];
 
+int print_size = 0;
+
 bool is_terminated = false;
 
 void print_map_exit(bool flag = false){
@@ -28,20 +30,25 @@ void print_map_exit(bool flag = false){
 }
 
 bool check_square(){
-    int width = 4;
-    int height = 4;
+    int row_info[MAP_SIZE] = {0, };
+    int col_info[MAP_SIZE] = {0, };
+    print_size = 0;
 
     for(int i = 0; i < MAP_SIZE; i++){
         for(int j = 0; j < MAP_SIZE; j++){
-            if(map[i][j] == 0){
-                width  = width < j ? width : j;
-                height = height < i ? height : i;
+            if(map[i][j] != 0){
+                row_info[j]++;
+                col_info[i]++;
             }
         }
     }
+
+    for(int i = 0; i < MAP_SIZE; i++){
+        if(row_info[i] != col_info[i]) return false;
+        print_size = row_info[i] > print_size ? row_info[i] : print_size;
+    }
     
-    printf("height::%d | width::%d\n", height, width);
-    return (height == width);
+    return true;
 }
 
 void del_poly(int idx_poly, int map_h, int map_w){
@@ -52,11 +59,9 @@ void del_poly(int idx_poly, int map_h, int map_w){
             }
         }
     }    
-    // cout << "Dell:: " << idx_poly <<endl;
-    // print_map();
 }
 
-bool sign_poly(int idx_poly, int map_y, int map_x){
+bool assign_poly(int idx_poly, int map_y, int map_x){
     int height = pairs[idx_poly].h;
     int width = pairs[idx_poly].w;
 
@@ -75,8 +80,8 @@ bool sign_poly(int idx_poly, int map_y, int map_x){
             }
         }
     }
-    cout << "Sign:: " << idx_poly + 1 <<endl;
-    print_map_exit();
+    // cout << "Sign:: " << idx_poly + 1 <<endl;
+    // print_map_exit();
     return true;
 }
 
@@ -87,7 +92,7 @@ void solve(int idx_poly){
 
     for(int i = 0; i < MAP_SIZE; i++){
         for(int j = 0; j < MAP_SIZE; j++){
-            is_signed = sign_poly(idx_poly, i, j);
+            is_signed = assign_poly(idx_poly, i, j);
 
             if(idx_poly == n - 1 && is_signed){
                 is_terminated = check_square();
