0f823fb754d3f301544d7246267dbe141bcabe0a
misoyuri <21600004@handong.edu>
2021-10-28 16:27:12 +0900

    C6 time limit version

--- C6_21600004.cpp
+++ C6_21600004.cpp
@@ -3,44 +3,63 @@
 using namespace std;
 
 #define MAX_INT 2147483647
+#define DEBUG_MODE false
 
 int deck[50001]{0, };
+int debug_deck[50001]{0, 1};
 int N = 0;
 
 int max_score = 0;
 int lower_card = MAX_INT;
 bool is_ascending = true;
 
-void dfs(int card_seq, int score){
+void print_debug_log(int score){
+    for(int i = 0; i < score; i++){
+        printf("%d(%d)", deck[debug_deck[i]], debug_deck[i]);
+        if(i != score - 1){
+            printf("->");
+        }else{
+            printf("\n");
+        }
+    }
+}
+
+void dfs(int card_seq, int score, int selected_card){
     if(card_seq == N){
+        // if(max_score < score) 
+        if(DEBUG_MODE) print_debug_log(score);
         max_score = max(max_score, score);
         return;
     }
 
-    dfs(card_seq + 1, score);
+    dfs(card_seq + 1, score, selected_card);
 
     if(score == 0){
-        dfs(card_seq + 1, score + 1);
-    }
+        if(DEBUG_MODE) debug_deck[score] = card_seq;
+        is_ascending = true;
+        dfs(card_seq + 1, score + 1, deck[card_seq]);
+    }else{
+        if(is_ascending == true){
+            if(selected_card < deck[card_seq]){
+                if(DEBUG_MODE) debug_deck[score] = card_seq;
+                dfs(card_seq + 1, score + 1, deck[card_seq]);
+            }else if(selected_card > deck[card_seq]){
+                lower_card = deck[card_seq];
+                is_ascending = false;
+                if(DEBUG_MODE) debug_deck[score] = card_seq;
+                dfs(card_seq + 1, score + 1, deck[card_seq]);
 
-
-
-    if(score != 0 && is_ascending){
-        if(deck[card_seq - 1] < deck[card_seq]){
-            dfs(card_seq + 1, score + 1);
-        }else if(deck[card_seq - 1] > deck[card_seq]){
-            is_ascending = false;
-            lower_card = deck[card_seq];
-            dfs(card_seq + 1, score + 1);
-            lower_card = MAX_INT;
-            is_ascending = true;
+                is_ascending = true;
+            }
         }
-
-    if(!is_ascending && lower_card > deck[card_seq]){
-            dfs(card_seq + 1, score + 1);
+        
+        if(is_ascending == false){
+            if(selected_card > deck[card_seq]){
+                if(DEBUG_MODE) debug_deck[score] = card_seq;
+                dfs(card_seq + 1, score + 1, deck[card_seq]);
+            }    
         }
     }
-    
 }
 
 int main(){
@@ -48,8 +67,8 @@ int main(){
     for(int i = 0; i < N; i++){
         cin >> deck[i];
     }
-
-    dfs(0, 0);
+    // cout << "---------------" << endl;
+    dfs(0, 0, 0);
     cout << max_score << endl;
     // cout << "ret::" << max_score << endl;
 }
