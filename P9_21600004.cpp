13161c3cc5423b53639a1167d973a53918457ec0
misoyuri <21600004@handong.edu>
2021-11-23 19:02:59 +0900

    P9 Completed!

--- P9_21600004.cpp
+++ P9_21600004.cpp
@@ -1,78 +1,71 @@
 #include <iostream>
 #include <vector>
-#include <algorithm>
-#include <stack>
 
 using namespace std;
 
 int N = 0;
 int B = 0;
 
-vector<int> cell_edges[450];
-vector<int> circle_components;
-
-stack<int> st;
+vector<int> cell_relations[450];
 int weights[450];
-bool visited_st[450];
-bool visited_cc[450];
 
-void DFS_for_stack(int target_node){
-    visited_st[target_node] = true;
+int max_weight_sum = 0;
 
-    for(int i = 0; i < cell_edges[target_node].size(); i++){
-        if(visited_st[cell_edges[target_node][i]] == true) continue;
+int calc_weighted_sum(vector<int> targets){
+    if(targets.size() > 4) return 0;
 
-        DFS_for_stack(cell_edges[target_node][i]);
+    int ret = 0;
+    // printf("[");
+    for(int i = 0; i < targets.size(); i++){
+        ret += weights[targets[i]];
+        // printf("%d ", targets[i]+1);
     }
-
-    st.push(target_node);
+    // printf("] %d\n", ret);
+    
+    return ret;
 }
 
-void DFS_for_CC(int target_node){
-    visited_cc[target_node] = true;
 
-    for(int i = 0; i < cell_edges[target_node].size(); i++){
-        if(visited_cc[cell_edges[target_node][i]] == true) continue;
+void find_fully_connected(int start_node){
+    vector<int> fully_connected;
 
-        DFS_for_CC(cell_edges[target_node][i]);
-    }
+    fully_connected.push_back(start_node);
 
-    circle_components.push_back(target_node);
-}
+    for(int v_id = 0; v_id < N; v_id++){
+        bool continue_flag = false;
+        bool append_flag = true;
 
-int calc_max_cc(){
-    int sum = 0;
-    cout << "[";
-    for(int i = 0; i < circle_components.size(); i++){
-        printf("%d ", circle_components[i]);
-        sum += weights[circle_components[i]];
-    }
-    cout << "]" << endl;;
-
-    circle_components.clear();
-
-    return sum;
-}
-
-int solve(){
-    int max_cc_sum = 0;
-    for(int i = 0; i < N; i++){
-        if(!visited_st[i]) DFS_for_stack(i);
-    }
-
-    while(!st.empty()){
-        int node = st.top();
-        st.pop();
-
-        if(!visited_cc[node]){
-            DFS_for_CC(node);
-            int ret = calc_max_cc();
-            if(ret > max_cc_sum) max_cc_sum = ret;
+        for(int fc_idx = 0; fc_idx < fully_connected.size(); fc_idx++){
+            if(v_id == fully_connected[fc_idx]){
+                continue_flag = true;
+                break;
+            }
         }
+
+        if(continue_flag) continue;
+
+        for(int fc_idx = 0; fc_idx < fully_connected.size(); fc_idx++){
+            continue_flag = false;
+            for(int adj_cell_to_v_id = 0; adj_cell_to_v_id < cell_relations[v_id].size(); adj_cell_to_v_id++){
+                if(fully_connected[fc_idx] == cell_relations[v_id][adj_cell_to_v_id]){
+                    continue_flag = true;
+                    break;
+                }
+            }
+
+            if(!continue_flag){
+                append_flag = false;
+                break;
+            }
+        }
+
+        if(append_flag)
+            fully_connected.push_back(v_id);
     }
 
+    int ret = calc_weighted_sum(fully_connected);
 
-    return max_cc_sum;
+    if(ret > max_weight_sum) max_weight_sum = ret;
 }
 
 int main(){
@@ -80,17 +73,21 @@ int main(){
     int cell_id_second = 0;
     cin >> N >> B;
 
-    for(int i = 1; i <= N; i++){
+    for(int i = 0; i < N; i++){
         cin >> weights[i];
     }
 
     for(int i = 0; i < B; i++){
         cin >> cell_id_first >> cell_id_second;
-        cell_edges[cell_id_first].push_back(cell_id_second);
-        cell_edges[cell_id_second].push_back(cell_id_first);
+        cell_relations[cell_id_first-1].push_back(cell_id_second-1);
+        cell_relations[cell_id_second-1].push_back(cell_id_first-01);
     }
 
-    cout << solve() << endl;
+    for(int i = 0; i < N; i++){
+        // printf("Start Node: %d | ", i);
+        find_fully_connected(i);
+    }
+    cout << max_weight_sum << endl;
 
     return 0;
 }
